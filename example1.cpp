04ad63eab75a8731353c5a61484797d5bc07b1ff
KaboomPhysicist(C Os) <cristian.serna1@udea.edu.co>
2023-04-26 11:54:40 -0500

    Added examples

---
+++ example1.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <iostream>
+
+#include "Hyperbolic_solver.h"
+
+constexpr double pi = 3.14159265358979323846;
+
+std::vector <float> time0;
+std::vector <float> position0;
+std::vector <std::vector <float>> solutions0;
+
+float f(float x){ return std::sin(x*pi);}
+float g(float x){ return 0;}
+
+const float example1_sol(float x,float t){ return std::sin(x*pi)*std::cos(pi*t);}
+
+
+int main(){
+    const HyperbolicSolver example1 ( f, g, 1, 1, 1, 10, 20);
+    ExactCompare example1_exact (example1, example1_sol);
+    //example1.print_table(POSITION, 4);
+    //example1.print_table(TIME, 20);
+
+    example1.print_bounds();
+    example1.print_y0();
+
+    time0 = example1.getTime();
+    position0 = example1.getPositions();
+    solutions0 = example1.getW();
+
+    std::cout << "Check of the numerical solution compared with the exact solution:\n";
+    example1_exact.print_comp_table(TIME, 20);
+    example1_exact.print_comp_table(POSITION, 4);
+    return 0;
+}
+
---
+++ example2.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <iostream>
+
+#include "Hyperbolic_solver.h"
+
+constexpr double pi = 3.14159265358979323846;
+
+std::vector <float> time0;
+std::vector <float> position0;
+std::vector <std::vector <float>> solutions0;
+
+const float p0 = 0.9;
+
+float f(float x){ return p0 * (std::cos(2*x*pi) - 1);}
+float g(float x){ return 0;}
+
+int main(){
+    const HyperbolicSolver example2 ( f, g, 1, 1, 1, 100, 200);
+    example2.print_table(POSITION, 40);
+    example2.print_table(TIME, 200);
+
+    example2.print_bounds();
+    example2.print_y0();
+
+    time0 = example2.getTime();
+    position0 = example2.getPositions();
+    solutions0 = example2.getW();
+
+    return 0;
+}
+
