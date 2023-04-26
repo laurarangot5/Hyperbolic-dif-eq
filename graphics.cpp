ac967ac61dcbca773453cdacbb41f0994213f025
larango <laura.arangot@udea.edu.co>
2023-04-26 11:47:52 -0500

    sclice_plot function ready, plot_heat map started

--- graphics.cpp
+++ graphics.cpp
@@ -1,49 +1,88 @@
 #include "Hyperbolic_solver.h"
 #include "matplotlibcpp.h"
 #include <vector>
+#include <iostream>
 
 namespace plt = matplotlibcpp;
 
 
-enum fixed_var{POSITION, TIME};
+//enum fixed_var{POSITION, TIME};
 
 
 graphics::graphics(const HyperbolicSolver& sol_object): 
                                 sol_object(sol_object){}
 
 void graphics::plot_slice(short int fix, float value_pos) const{
+    /*  This function selects either a colum or a row of the matriz W and plots it against the time or position
+        depending on the value of the variable fixed.
+        For the rows value_pos < m+1 and for the columns value_pos < n+1
+    */
+
+    int n = sol_object.get_n();
+    int m = sol_object.get_m();
 
     std::vector <float> time = sol_object.getTime();
     std::vector <float> pos = sol_object.getPositions();
     if (fix == POSITION){
-
-        //here we get from the matrix w, a column that represents the times for that solution
         
-        std::vector <float> w = sol_object.getW();
+        std::vector <std::vector <float>> w = sol_object.getW();
         std::vector <float> W;
-        for(int i = 0; i < n+1; i++){
-                W.push_back(w.at(i).at(value_pos));
-        }
-        delete[] w;
+        std::vector <float> t;
 
-        plt::plot(time, W);
+        for(int i = 0; i < m+1; i++){
+                W.push_back(w.at(i).at(value_pos));
+                t.push_back(time.at(i));
+                
+        }
+
+        plt::plot(t, W);
         plt::title("Solution of the differential equation at a the fixed position "+std::to_string(pos.at(value_pos)));
         plt::xlabel("Time");
-        plt::ylabel("Solution");
+        plt::ylabel("W ("+std::to_string(pos.at(value_pos))+",j)");
         plt::show();
     }
 
     else if (fix == TIME){
         
-        std::vector <float> W = sol_object.getW().at(value_pos);
+        std::vector <float> w = sol_object.getW().at(value_pos);
+        std::vector <float> W;
+
+        for(int i = 0; i < m+1; i++){
+            W.push_back(w.at(i));}
         
 
         plt::plot(pos, W);
         plt::title("Solution of the differential equation for the fixed time "+std::to_string(time.at(value_pos)));
         plt::xlabel("Position");
-        plt::ylabel("Solution");
+        plt::ylabel("W (i,"+std::to_string(time.at(value_pos))+")");
         plt::show();
+        
     }
 }
+
+void graphics::plot_heatmap() const{
+    /*  This function plots the matrix W as a heatmap
+    */
+    std::vector <std::vector <float>> w = sol_object.getW();
+    std::vector <float> time = sol_object.getTime();
+    std::vector <float> pos = sol_object.getPositions();
+
+    plt::figure();
+    plt::imshow(w);
+    plt::xticks(time);
+    plt::yticks(pos);
+    plt::show();
+
+
+/*
+    plt::imshow();
+    plt::title("Solution of the differential equation");
+    plt::xlabel("Position");
+    plt::ylabel("Time");
+    plt::zlabel("W (i,j)");
+    plt::show();
+*/ 
+}
+
 
 
---
+++ plot.cpp
@@ -0,0 +1,26 @@
+#include "Hyperbolic_solver.h"
+#include <cmath>
+
+constexpr double pi = 3.14159265358979323846;
+
+float f(float x){ return std::sin(x*pi);}
+float g(float x){ return 0;}
+
+float example0_sol(float x,float t){ return std::sin(x*pi)*std::cos(2*pi*t);}
+
+
+int main(){
+
+
+
+    const HyperbolicSolver example0 ( f, g, 2, 1, 1, 10, 20);
+    //example0.print_table(POSITION, 4);
+    example0.print_table(TIME, 20);
+
+    graphics example0_graph (example0);
+    //example0_graph.plot_slice(POSITION, 4);
+    //example0_graph.plot_slice(TIME, 2);
+    example0_graph.plot_heatmap();
+
+    return 0;
+}
