69d28a909202a50c4e8e5073c0bcdb2c744af63c
KaboomPhysicist <cristian.serna1@udea.edu.co>
2023-04-25 23:22:58 -0500

    Secondary functions testing

--- main.cpp
+++ main.cpp
@@ -1,21 +1,40 @@
 #include "Hyperbolic_solver.h"
 #include <cmath>
+#include <iostream>
 
 constexpr double pi = 3.14159265358979323846;
+
+std::vector <float> time0;
+std::vector <float> position0;
+std::vector <std::vector <float>> solutions0;
 
 float f(float x){ return std::sin(x*pi);}
 float g(float x){ return 0;}
 
 float example0_sol(float x,float t){ return std::sin(x*pi)*std::cos(2*pi*t);}
 
-
 int main(){
-
-
-
     HyperbolicSolver  example0 ( f, g, 2, 1, 1, 10, 20);
     //example0.print_table(POSITION, 4);
-    example0.print_table(TIME, 20);
+    //example0.print_table(TIME, 20);
+
+    std::cout << "Check of secondary functions:\n";
+
+    std::cout << "m = ";
+    example0.print_m();
+    std::cout << "n = ";
+    example0.print_n();
+    std::cout << "alpha = ";
+    example0.print_alpha();
+    example0.print_bounds();
+    example0.print_y0();
+
+    time0 = example0.getTime();
+    position0 = example0.getPositions();
+    solutions0 = example0.getW();
+
+    std::cout << "Check of the numerical solution compared with the exact solution:\n";
 
     return 0;
 }
+
