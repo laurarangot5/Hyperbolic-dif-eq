2c48d615fb2a41c59af04bbbf310bce7cf285ca0
KaboomPhysicist <84940951+KaboomPhysicist@users.noreply.github.com>
2023-04-26 08:19:37 -0500

    Included modified table.

--- exact_compare.cpp
+++ exact_compare.cpp
@@ -12,6 +12,7 @@ void ExactCompare::calculateValues(short int fix, float value_pos){
     //Get the mesh
     tiempos = sol_object.getTime();
     posiciones = sol_object.getPositions();
+    aproximaciones = sol_object.getW();
 
     // Vector for the exact solution in the mesh
     w = std::vector <std::vector <float>> (posiciones.size(), std::vector <float> (tiempos.size()));
@@ -26,8 +27,43 @@ void ExactCompare::calculateValues(short int fix, float value_pos){
 void ExactCompare::print_comp_table(short int fix, float value_pos){
     /*Prints the comparison table for a fixed time or position (index)*/
 
-    if(fix == POSITION){
-	std::cout << "Position fixed at:"
+     if(fix == POSITION){
+
+        std::cout << verdecito_chimbita << "Position fixed at: " << std::endl;
+        std::cout << "Index: " << std::defaultfloat << value_pos << std::endl;
+        std::cout << "Position: " << std::setprecision(7) << std::fixed << posiciones.at(value_pos) << std::endl << std::endl;
+        std::cout << std::left << purple << std::setw(15) << "Time" << std::setw(2) << std::left << "W(" << std::defaultfloat << value_pos << ",j)" << std::endl;
+	    std::cout << "----------------------------" << std::endl;
+        std::cout << reset;
+        for(int i = 0; i < n+1; i++){
+            std::cout << std::setw(15) << std::setprecision(7) << std::fixed << tiempos.at(i) << std::setw(15) << std::setprecision(7) << aproximaciones.at(value_pos).at(i) << std::setw(15) << std::setprecision(7) << w.at(value_pos).at(i) << std::endl;
+        }
+	    std::cout << purple << "----------------------------" << std::endl;
+        std::cout << reset;
+        std::cout << std::endl;
+
+    std::cout << std::endl;
+
+    }
+    else if(fix == TIME){
+        std::cout << verdecito_chimbita << "Time fixed at: " << std::endl;
+        std::cout << "Index: " << std::defaultfloat << value_pos << std::endl;
+        std::cout << "Time: " << std::setprecision(7) << std::fixed << tiempos.at(value_pos) << std::endl << std::endl;
+        std::cout << std::left << purple << std::setw(15) << "Position" << std::setw(2) << std::left << "W(i," << std::defaultfloat << value_pos << ")" << "Exact value" << std::endl;
+	    std::cout << "----------------------------" << std::endl;
+	    std::cout << reset;
+        for(int i = 0; i < m+1; i++){
+            std::cout << std::setw(15) << std::setprecision(7) << std::fixed << std::left << posiciones.at(i) << std::setw(15) << aproximaciones.at(i).at(value_pos) << std::setw(15) << w.at(i).at(value_pos) << std::endl; 
+	}
+        std::cout << std::endl;
+	    std::cout << purple << "----------------------------" << std::endl;
+        std::cout << reset;
+        std::cout << std::endl;
+
+    }
+    
+    else{
+        std::cout << "Error: Invalid option" << std::endl;
     }
     
 }
