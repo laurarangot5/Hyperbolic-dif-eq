65d6aa920376ce56262a04e3d566ca4b1aa67787
KaboomPhysicist <cristian.serna1@udea.edu.co>
2023-04-27 20:35:46 -0500

    Reordered functioncs and variables

--- include/HyperbolicSolver.h
+++ include/HyperbolicSolver.h
@@ -6,14 +6,19 @@ enum fixed_var{POSITION, TIME};
 class HyperbolicSolver{
     public:
         HyperbolicSolver(float (*)(float), float (*)(float),float, float, float, const unsigned int, const unsigned int);
+
         std::vector <float> getTime() const;
         std::vector <float> getPositions() const;
         std::vector <std::vector <float>> getW() const;
+
+        // Get functions
         int get_m() const;
         int get_n() const;
         float get_alpha() const;
         float get_l() const;
         float get_T() const;
+
+        // Print functions
         void print_m() const;
         void print_n() const;
         void print_alpha() const;
@@ -22,20 +27,21 @@ class HyperbolicSolver{
         void print_table(short int, float) const;
     
     private:
+        // Solve functions
         void solve(float (*)(float), float (*)(float));
         void setInitialConditions(float (*)(float), float (*)(float));
         void matrixMultiplication();
-        std::vector <float> t;
-        std::vector <float> x;
+
         float lambda;
         float alpha;
         const unsigned int m;
         const unsigned int n;
-        std::vector <std::vector <float>> w; //(const unsigned int, std::vector<float>);
         float l;
         float T;
         float h;
         float k;
+
+        std::vector <std::vector <float>> w;
         std::vector <float> tiempos;
         std::vector <float> posiciones;
 
