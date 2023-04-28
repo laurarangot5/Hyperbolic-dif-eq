2b4ab98365a4eb374311c95bc7de226e04d7fe6c
Laura Arango <laura.arangot@udea.edu.co>
2023-04-28 12:42:23 -0500

    fixed indexes in graphics.cpp

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -22,25 +22,26 @@ void graphics::plot_slice(short int fix, float value_pos) const{
     //int n = sol_object.get_n();
     //int m = sol_object.get_m();
 
-    std::vector <float> time = sol_object.getTime();
+    std::vector <float> Time = sol_object.getTime();
     std::vector <float> pos = sol_object.getPositions();
 
     const int m = pos.size() - 1;
+    const int n = Time.size() - 1;
 
     if (fix == POSITION){
+
+        //the fixed poition index must be smaller than m
         
         std::vector <std::vector <float>> w = sol_object.getW();
         std::vector <float> W;
-        std::vector <float> t;
 
         // Extracts a column of the matrix W corresponding to the fixed position
-        for(int i = 0; i < m+1; i++){
-                W.push_back(w.at(i).at(value_pos));
-                t.push_back(time.at(i));
+        for(int i = 0; i < n+1; i++){
+                W.push_back(w.at(value_pos).at(i));
                 
         }
 
-        plt::plot(t, W);
+        plt::plot(Time, W);
         plt::title("Solution of the differential equation at a the fixed position "+std::to_string(pos.at(value_pos)));
         plt::xlabel("Time");
         plt::ylabel("W ("+std::to_string(pos.at(value_pos))+",j)");
@@ -48,19 +49,20 @@ void graphics::plot_slice(short int fix, float value_pos) const{
     }
 
     else if (fix == TIME){
-        
-        std::vector <float> w = sol_object.getW().at(value_pos);
+
+        //the fixed time index must be smaller than n
+        std::vector <std::vector <float>> w = sol_object.getW();
         std::vector <float> W;
 
         for(int i = 0; i < m+1; i++){
-            W.push_back(w.at(i));}
+            W.push_back(w.at(i).at(value_pos));}
         
 
         plt::plot(pos, W);
-        plt::title("Solution of the differential equation for the fixed time "+std::to_string(time.at(value_pos)));
+        plt::title("Solution of the differential equation for the fixed time "+std::to_string(Time.at(value_pos)));
         plt::xlabel("Position");
         // Consider using sstream
-        plt::ylabel("W (i,"+std::to_string(time.at(value_pos))+")");
+        plt::ylabel("W (i,"+std::to_string(Time.at(value_pos))+")");
         plt::show();
         
     }
@@ -70,13 +72,23 @@ void graphics::plot_heatmap() const{
     /*  This function plots the matrix W as a heatmap
     */
     std::vector <std::vector <float>> w = sol_object.getW();
-    std::vector <float> time = sol_object.getTime();
+    std::vector <float> Time = sol_object.getTime();
     std::vector <float> pos = sol_object.getPositions();
 
+    //we plot the matrix W as a heatmap
+    // we adjust the size of the figure
+    
+    plt::figure_size(1200, 780);
     plt::figure();
     plt::imshow(w);
-    plt::xticks(time);
+
+    //we adjust the ticks to fit the figure
+    
+    plt::xticks(Time);
     plt::yticks(pos);
+
+    //we add a colorbar
+    plt::colorbar();
     plt::show();
 
 
