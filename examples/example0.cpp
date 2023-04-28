6d3168b712ed357c6da070cb6f916c1717ec60ae
KaboomPhysicist <cristian.serna1@udea.edu.co>
2023-04-27 20:31:33 -0500

    Added graphics

--- examples/example0.cpp
+++ examples/example0.cpp
@@ -3,6 +3,7 @@
 
 #include "HyperbolicSolver.h"
 #include "ExactCompare.h"
+#include "graphics.h"
 
 constexpr double pi = 3.14159265358979323846;
 
@@ -20,6 +21,7 @@ const float example0_sol(float x,float t){ return std::sin(x*pi)*std::cos(2*pi*t
 int main(){
     const HyperbolicSolver example0 ( f, g, 2, 1, 1, 10, 20);
     ExactCompare example0_exact (example0, example0_sol);
+    graphics example0_graph (example0);
     //example0.print_table(POSITION, 4);
     //example0.print_table(TIME, 20);
 
@@ -41,6 +43,8 @@ int main(){
     std::cout << "Check of the numerical solution compared with the exact solution:\n";
     example0_exact.print_comp_table(TIME, 20);
     example0_exact.print_comp_table(POSITION, 4);
+
+    example0_graph.plot_slice(POSITION, 4);
     return 0;
 }
 
--- examples/example2.cpp
+++ examples/example2.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #include "HyperbolicSolver.h"
+#include "graphics.h"
 
 constexpr double pi = 3.14159265358979323846;
 
@@ -16,16 +17,13 @@ float g(float x){ return 0;}
 
 int main(){
     const HyperbolicSolver example2 ( f, g, 1, 1, 1, 100, 200);
-    example2.print_table(POSITION, 40);
-    example2.print_table(TIME, 200);
+    graphics example2_graphics(example2); 
 
     example2.print_bounds();
     example2.print_y0();
 
-    time0 = example2.getTime();
-    position0 = example2.getPositions();
-    solutions0 = example2.getW();
-
+    example2.print_table(TIME, 200);
+    example2_graphics.plot_heatmap();
     return 0;
 }
 
