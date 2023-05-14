7149aef6ffc61ab8131435d3745b68bffc827510
KaboomPhysicist(C Os) <cristian.serna1@udea.edu.co>
2023-05-14 16:16:11 -0500

    Full functions

--- examples/example0.cpp
+++ examples/example0.cpp
@@ -22,30 +22,23 @@ int main(){
     const HyperbolicSolver example0 ( f, g, 2, 1, 1, 10, 20);
     ExactCompare example0_exact (example0, example0_sol);
     graphics example0_graph (example0);
-    //example0.print_table(POSITION, 4);
-    //example0.print_table(TIME, 20);
+    example0.print_table(POSITION, 4);
+    example0.print_table(TIME, 10);
 
     std::cout << "Check of secondary functions:\n";
 
-    std::cout << "m = ";
-    example0.print_m();
-    std::cout << "n = ";
-    example0.print_n();
-    std::cout << "alpha = ";
-    example0.print_alpha();
-    example0.print_bounds();
-    example0.print_y0();
+    example0.info();
 
     time0 = example0.getTime();
     position0 = example0.getPositions();
     solutions0 = example0.getW();
 
     std::cout << "Check of the numerical solution compared with the exact solution:\n";
-//  example0_exact.print_comp_table(TIME, 10);
+    example0_exact.print_comp_table(TIME, 10);
     example0_exact.print_comp_table(POSITION, 4);
 
-//  example0_graph.plot_slice(POSITION, 4);
-//  example0_graph.plot_slice(example0_exact, POSITION, 4);
+    example0_graph.plot_slice(POSITION, 4);
+    example0_graph.plot_slice(example0_exact, POSITION, 4);
     example0_graph.plot_heatmap();
     example0_graph.plot_heatmap(example0_exact);
 
